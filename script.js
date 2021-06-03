4a02f3b1063e8cd2ca3eb78b1e14bb65d361b152
oksana <opashkovska85@gmail.com>
2021-06-04 00:25:07 +0200

    adding fetch request

---
+++ script.js
@@ -0,0 +1,23 @@
+const userName = window.location.search.split('=')[1];
+console.log(userName);
+
+fetch(`https://api.github.com/users/${userName}`)
+  .then(res => res.json())
+  .then(json => {
+    console.log(json);
+    let name = json.login;
+    let avatar = json.avatar_url;
+    let bio = json.bio;
+    let link = json.html_url;
+    console.log(name, avatar, bio, link);
+    if (name !== undefined) {
+      document.getElementById('avatar').setAttribute('src', avatar);
+      document.getElementById('name').innerHTML = `<a href='${link}' target=blank>${name}</a>`;
+      document.getElementById('bio').innerHTML = bio;
+    } else {
+      document.body.innerHTML="Информация о пользователе не доступна";
+    }
+  })
+  .catch(function(error) {
+    console.log("Информация о пользователе не доступна");
+  });
